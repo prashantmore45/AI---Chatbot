3d622d4112e400cdb7e9a2748c86a427ede76ff5
prashantmore45 <prashantmorepm05@gmail.com>
2025-09-15 17:38:18 +0530

    feat: render user messages inside chat container

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
 let userMessage = "";
 
+// Function to create message elements
+const createMsgElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+    div.innerHTML = content;
+    return div;
+}
 
+// Handle the Form submission
 const handleFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
     if (!userMessage) return;
 
-    console.log(userMessage);
+    // Generate user message HTML and add in the chats container
+    const userMsgHTML = `<p class="message-text"></p>`;
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
+    userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
 };
 
 promptForm.addEventListener("submit", handleFormSubmit);
